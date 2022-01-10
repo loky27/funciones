7fdcd37ec74f1a2a2cafcdf73cbebb5fd4727802
jonatha <hef6666@gmail.com>
2022-01-10 12:50:44 -0600

    first commit

---
+++ script,.js
@@ -0,0 +1,20 @@
+// Area del circulo 
+function circulo (radio,pi,resultado)
+{ var radio = 10; 
+    var pi = 3.1416 
+    var resultado = (radio * radio) * pi; 
+    alert ("el area del circulo es: " + resultado); } 
+circulo(); 
+// area de rectangulo 
+function rectangulo (base,altura,resultado){
+     var base = 15; 
+     var altura = 25; 
+     var resultado = base * altura; 
+     console.log(resultado); } 
+rectangulo(); 
+// area de triangulo 
+function triangulo (base,altura,resultado){
+     var resultado = (base * altura) / 2; 
+     alert ("el area del triangulo es: " + resultado); 
+    } 
+triangulo(5,10);
